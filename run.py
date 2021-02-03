d8036ef7da261320d38c14829fdf65bd9a8595d7
Dai Wei <nbdavied@gmail.com>
2021-02-03 10:56:14 +0800

    查找银行账单和随手记账单的对应关系

--- run.py
+++ run.py
@@ -11,6 +11,39 @@ def createBankReader(filename, path):
     if filename[:3] == 'abc':
         return ABCReader(config, os.path.join(path, filename))
     return None
+def determTransType(bankDetail):
+    if bankDetail["amount"][0] == '+':
+        return 'income'
+    if bankDetail["amount"][0] == '-':
+        return 'payout'
+    return None
+def determAmount(bankDetail, suiDetail):
+    bAmt = bankDetail["amount"][1:]
+    sAmt = format(suiDetail["itemAmount"], '0.2f')
+    return bAmt == sAmt
+def isMatchedDetail(bankDetail, suiDetail, suiid):
+    if bankDetail["date"] != suiDetail['sdate']:
+        return False
+    if not determAmount(bankDetail, suiDetail):
+        return False
+    transType = determTransType(bankDetail)
+    if transType == 'income':
+        if suiDetail['tranType'] == 5:
+            return True
+        if suiDetail['tranType'] == 2 and suiDetail['sellerAcountId'] == suiid:
+            return True
+    if transType == 'payout':
+        if suiDetail['tranType'] == 1:
+            return True
+        if suiDetail['tranType'] == 2 and suiDetail['buyerAccountId'] == suiid:
+            return True
+    return False
+    
+def findSuiDetail(bankDetail, suiDetails, suiid):
+    for suiDetail in suiDetails:
+        if isMatchedDetail(bankDetail, suiDetail, suiid):
+            return suiDetail
+    return None
 
 config = None
 if __name__ == "__main__":
@@ -34,4 +67,5 @@ if __name__ == "__main__":
         bankDetails = bank['details']
         print(bankDetails)
         for bankDetail in bankDetails:
-            pass
+            transType = determTransType(bankDetail)
+
